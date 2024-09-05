5fddd6eab1c20cebfa588a0641151d5c9976c5c7
Yauheni Shydlouski <yauheni.shydlouski@iubenda.com>
2024-09-06 01:07:00 +0700

    day 2

--- lib/t_and_c_generator.ex
+++ lib/t_and_c_generator.ex
@@ -1,8 +1,33 @@
 defmodule TAndCGenerator do
   def generate do
-    _template = load_file("template.txt")
-    _sections = load_json(:sections)
-    _clauses = load_json(:clauses)
+    template = load_file("template.txt")
+    template = replace_sections(template)
+    template = replace_clauses(template)
+
+    IO.puts(template)
+  end
+
+  def replace_clauses(template) do
+    clauses = load_json(:clauses)
+
+    Enum.reduce(clauses, template, fn clause, acc ->
+      String.replace(acc, "[CLAUSE-#{clause["id"]}]", clause["text"])
+    end)
+  end
+
+  def replace_sections(template) do
+    sections = load_json(:sections)
+
+    Enum.reduce(sections, template, fn section, acc ->
+      clauses = section_clauses(section["clauses_ids"])
+      String.replace(acc, "[SECTION-#{section["id"]}]", clauses)
+    end)
+  end
+
+  def section_clauses(clauses_ids) do
+    clauses_ids
+    |> Enum.map(fn clause_id -> "[CLAUSE-#{clause_id}]" end)
+    |> Enum.join(";")
   end
 
   def load_file(file) do
