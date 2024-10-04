35a16a913900bf3da07324aecb8d46c87f0f7274
Yauheni Shydlouski <yauheni.shydlouski@iubenda.com>
2024-10-04 18:19:51 +0700

    mappings

--- lib/mix/tasks/generate.ex
+++ lib/mix/tasks/generate.ex
@@ -4,16 +4,17 @@ defmodule Mix.Tasks.Generate do
   @shortdoc "Calls the generate function from the TAndCGenerator module"
 
   def run(args) do
-    [template, sections, clauses] = fill_defaults(args)
+    [template, sections, clauses, mapping] = fill_defaults(args)
 
     TAndCGenerator.generate(
       template || "./data/template.txt",
       sections || "./data/sections.json",
-      clauses || "./data/clauses.json"
+      clauses || "./data/clauses.json",
+      mapping || "./data/mapping.json"
     )
   end
 
   defp fill_defaults(args) do
-    args ++ List.duplicate(nil, 3 - length(args)) |> Enum.take(3)
+    args ++ List.duplicate(nil, 4 - length(args)) |> Enum.take(4)
   end
 end
