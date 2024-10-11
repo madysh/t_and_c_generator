ef2edded1b51fdddf86b10ff31cf9ae8334b6a56
Yauheni Shydlouski <yauheni.shydlouski@iubenda.com>
2024-10-11 14:33:00 +0700

    fixes

--- lib/modules/mapping.ex
+++ lib/modules/mapping.ex
@@ -1,14 +1,14 @@
 defmodule Mapping do
   def replace(text, mappings) do
-    Enum.each(mappings, fn {k, v} ->
-      replace_key(text, [k], v)
+    Enum.reduce(mappings, text, fn {k, v}, acc ->
+      replace_key(acc, [k], v)
     end)
   end
 
   defp replace_key(text, keys, value) do
     if is_map(value) do
-      Enum.each(value, fn {k, v} ->
-        replace_key(text, keys++[k], v)
+      Enum.reduce(value, text, fn {k, v}, acc ->
+        replace_key(acc, keys ++ [k], v)
       end)
     else
       String.replace(text, "@{ #{Enum.join(keys, ".")} }", Integer.to_string(value))
