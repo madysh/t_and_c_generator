5ea6a0e78f7f0110df5e070041df087311aabc4c
Yauheni Shydlouski <yauheni.shydlouski@iubenda.com>
2024-10-11 17:37:09 +0700

    replace_misseing_tags fix

--- lib/modules/clause.ex
+++ lib/modules/clause.ex
@@ -6,16 +6,30 @@ defmodule Clause do
   def replace(template, clauses, mapping) do
     clauses = FileManager.load_json(clauses)
     mappings = FileManager.load_json(mapping)
+    clause_ids = Enum.map(clauses, fn x -> x["id"] end)
 
     Enum.reduce(clauses, template, fn clause, acc ->
       value = Mapping.replace(clause["text"], mappings)
 
       String.replace(acc, "[#{marker()}-#{clause["id"]}]", value, global: false)
     end)
-    |> replace_misseing_tags()
+    |> replace_misseing_tags(clause_ids)
   end
 
-  defp replace_misseing_tags(template) do
-    String.replace(template, ~r/\[(CLAUSE)-(\d+)\]/, "")
+  defp replace_misseing_tags(template, clause_ids) do
+    String.replace(template, ~r/\[CLAUSE-(\d+)\]/, fn match ->
+      case Regex.run(~r/\d+/, match) do
+        [digit] ->
+          id = String.to_integer(digit)
+
+          if id in clause_ids do
+            match
+          else
+            ""
+          end
+        _ ->
+          match
+      end
+    end)
   end
 end
