a9902042060f85f8b3d8dbaa5614ad43a36d4f91
Federico Barbazza <federico.barbazza@iubenda.com>
2024-10-25 12:23:45 +0200

    fix tests

--- test/modules/clause_test.exs
+++ test/modules/clause_test.exs
@@ -3,7 +3,7 @@ defmodule ClauseTest do
 
   test "replaces a clause" do
     template = "This is a [CLAUSE-1] [CLAUSE-100]."
-    expected = "This is a The quick brown fox [CLAUSE-100]."
+    expected = "This is a The quick brown @{ count } foxes [CLAUSE-100]."
 
     assert Clause.replace(template, "./data/clauses.json") == expected
   end
--- test/modules/section_test.exs
+++ test/modules/section_test.exs
@@ -3,7 +3,7 @@ defmodule SectionTest do
 
   test "replaces a section" do
     template = "This is a [SECTION-1] [SECTION-100]."
-    expected = "This is a [CLAUSE-1];[CLAUSE-2] [SECTION-100]."
+    expected = "This is a [CLAUSE-1];[CLAUSE-2] ."
 
     assert Section.replace(template, "./data/sections.json") == expected
   end
